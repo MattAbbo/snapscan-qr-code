3a049fe024ba77d5a5b6595be457557104ac6b8a
Matt Aberdein <matt@snapscan.co.za>
2016-02-03 12:11:53 +0200

    Revert "fixed order of javascript"
    This reverts commit b2967f28ed35081de7aa0e952d1bfbcc0a945c92.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -10,7 +10,44 @@
 // SMS text for Mr Delivery - probably pre-filled based on deploy?
 // change
 // <script src="scripts/vendor/qrcode.min.js">
-<script src="scripts/app.js"> */
+// <script src="scripts/app.js"> */
+
+// whatsapp share button
+$(document).ready(function() {
+var isMobile = {
+Android: function() {
+    return navigator.userAgent.match(/Android/i);
+},
+BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+},
+iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+},
+Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+},
+Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+},
+any: function() {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+}
+        };
+$(document).on("click", '.whatsapp', function() {
+    if( isMobile.any() ) {
+
+        var text = $(this).attr("data-text");
+        var url = $(this).attr("data-link");
+        var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
+        var whatsapp_url = "whatsapp://send?text=" + message;
+        window.location.href = whatsapp_url;
+    } else {
+        alert("Please share this article in mobile device");
+    }
+
+});
+});
 
 //button : create link
 $('#generateButton').click(function() {
